078df10ad679f622355eb77ace0bbeaf24532fcf
skskdmsdl <skskdmsdl@gmail.com>
2021-07-04 13:55:54 +0900

    자료형

---
+++ 01. Data Type/3. boolean.py
@@ -0,0 +1,9 @@
+# 참/거짓(Boolean) 자료형
+
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
---
+++ 01. Data Type/4. variable.py
@@ -0,0 +1,14 @@
+# 변수 자료형
+
+# 애완동물 소개하기
+animal = "고양이"
+name = "효리"
+age = 6
+like = "낮잠"
+is_adult = age >= 3
+
+print("우리집" + animal + "의 이름은 " + name + "입니다")
+like = "먹을 것"
+# print(name + "는 " + str(age) + "살이고, " + like + "을 아주 좋아합니다.")
+print(name, "는", age, "살이고,", like, "을 아주 좋아합니다.")
+print(name + "는 어른일까요? " + str(is_adult))
