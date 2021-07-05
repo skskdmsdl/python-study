0fde869d82e11e088c980c26394918aaabc9d7c9
skskdmsdl <skskdmsdl@gmail.com>
2021-07-05 21:41:23 +0900

    연산자 공부

---
+++ 02. Operator/3.2 function.py
@@ -0,0 +1,13 @@
+# 숫자처리 함수
+
+print(abs(-5)) # 절대값 5
+print(pow(4, 2)) # 4^2 = 4*4 = 16
+print(max(5, 12)) # 최대값 12
+print(min(5, 12)) # 최소값 5
+print(round(3.14)) # 반올림 3
+print(round(4.99)) # 반올림 5
+
+from math import *
+print(floor(4.99)) # 내림 4
+print(ceil(3.14)) # 올림 4
+print(sqrt(16)) # 제곱근 4
