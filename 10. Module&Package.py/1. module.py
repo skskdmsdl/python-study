97a8f47981900d73b68c011eb0de108cb8e7a9e5
skskdmsdl <skskdmsdl@gmail.com>
2021-07-22 20:53:35 +0900

    모듈 공부

---
+++ 10. Module&Package.py/1. module.py
@@ -0,0 +1,24 @@
+# 모듈
+
+# import theater_module
+# theater_module.price(3) # 3명이서 영화 보러 갔을 때 가격
+# theater_module.price_morning(4) # 4명이서 조조 할인 영화 보러 갔을 때 가격
+# theater_module.price_soldier(5) # 5명이서 군인 할인 영화 보러 갔을 때 가격
+
+# import theater_module as mv
+# mv.price(3) # 3명이서 영화 보러 갔을 때 가격
+# mv.price_morning(4) # 4명이서 조조 할인 영화 보러 갔을 때 가격
+# mv.price_soldier(5) # 5명이서 군인 할인 영화 보러 갔을 때 가격
+
+# from theater_module import *
+# price(3) 
+# price_morning(4)
+# price_soldier(5)
+
+# from theater_module import price, price_morning
+# price(3) 
+# price_morning(4)
+# # price_soldier(5)
+
+from theater_module import price_soldier as price
+price(5) 
