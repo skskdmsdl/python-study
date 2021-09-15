9c27aa9c81bd7453c733a075eeb7a6744cb71414
skskdmsdl <66931820+skskdmsdl@users.noreply.github.com>
2021-09-15 21:48:18 +0900

    pandas 데이터 수정 삭제 study

--- 13. Data Analysis Lib/2. pandas.py
+++ 13. Data Analysis Lib/2. pandas.py
@@ -34,10 +34,7 @@ country = pd.DataFrame({
 
 # Dictionary를 활용하여 DataFrame 생성 가능
 data = {
-  'country': ['china', 'japan',
-  'korea'
-  ,
-  'usa'],
+  'country': ['china', 'japan', 'korea',  'usa'],
   'gdp': [1409250000, 516700000, 169320000, 2041280000],
   'population': [141500, 12718, 5180, 32676]
 }
@@ -97,3 +94,21 @@ country.query("population > 100000") # query 함수 활용
 # Series도 numpy array처럼 연산자 활용 가능
 gdp_per_capita = country['gdp'] / country['population']
 country['gdp per capita'] = gdp_per_capita
+
+
+# 리스트로 추가 or 딕셔너리로 추가
+df = pd.DataFrame(columns = ['이름', '나이', '주소']) # 데이터프레임 생성
+df.loc[0] = ['길동', '26', '서울'] # 리스트로 데이터 추가
+df.loc[1] = {'이름':'철수', '나이':'25', '주소':'인천'} # 딕셔너리로 데이터 추가
+df.loc[1, '이름'] = '영희' # 명시적 인덱스 활용하여 데이터 수정
+
+
+# NaN값으로 초기화 한 새로운 컬럼 추가
+df['전화번호'] = np.nan # 새로운 컬럼 추가 후 초기화
+df.loc[0, '전화번호'] = '01012341234' # 명시적 인덱스 활용하여 데이터 수정
+
+
+# DataFrame에서 컬럼 삭제 후 원본 변경
+df.drop('전화번호', axis = 1, inplace = True) # 컬럼 삭제
+# axis = 1 : 열 방향 / axis = 0 : 행 방향
+# inplace = True : 원본 변경 / inplace = False : 원본 변경 X
