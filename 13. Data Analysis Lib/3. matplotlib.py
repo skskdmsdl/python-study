be89f6dda57f477fad8fdcac2ee4b9209ddc0463
skskdmsdl <66931820+skskdmsdl@users.noreply.github.com>
2021-10-01 21:56:10 +0900

    Marker study

--- 13. Data Analysis Lib/3. matplotlib.py
+++ 13. Data Analysis Lib/3. matplotlib.py
@@ -31,3 +31,12 @@ ax.plot(x, x, color="r")
 ax.plot(x, x+2, color="green")
 ax.plot(x, x+4, color="0.8")
 ax.plot(x, x+6, color="#524FA1")
+
+# Marker
+x = np.arange(10)
+fig, ax = plt.subplots()
+ax.plot(x, x, marker=".")
+ax.plot(x, x+2, marker="o")
+ax.plot(x, x+4, marker="v")
+ax.plot(x, x+6, marker="s")
+ax.plot(x, x+8, marker="*")
